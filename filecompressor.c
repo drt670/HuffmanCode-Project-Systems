815906b91d93025a1d7d5d660d2a034c0b79c3a4
Daniel Tolentino <drt67@rutgers.edu>
2019-03-24 21:17:17 -0400

    added h file

---
+++ filecompressor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct node{
+	int data;
+	struct node *left;
+	struct node *right;
+}node;
+
+int main() {
+
+	FILE *ptr;
+
+}
