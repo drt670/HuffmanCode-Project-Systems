d3ed9fcb2b86842b2d9e2e2fe06bb6506c401466
Daniel Tolentino <drt67@rutgers.edu>
2019-03-26 22:36:37 -0400

    recursive list files

--- openDIR.c
+++ openDIR.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+
+void listFilesRecursively(char *path);
 
 int main() {
 
-	struct dirent *ptr;
+	char path[100] = ".";
 
-	DIR *open = opendir("."):
+	listFilesRecursively(path);
 
-	if(open == NULL {
-		printf("could not open current directory);
-		return 0;
-	}
-
-	while ((ptr = readdir(open)) != NULL)
-		printf("%s\n", ptr->d_name);
-
-	closedir(open);
 	return 0;
+
 }
+
+
+void listFilesRecursively(char *basePath) {
+
+	char path[1000];
+	struct dirent *ptr;
+	DIR *dir = opendir(basePath);
+
+	// Unable to open directory
+	if (!dir)
+		return;
+
+	while ((ptr = readdir(dir)) != NULL) 
+	{
+		if (strcmp(ptr->d_name,".") != 0 && strcmp(ptr->d_name, "..") != 0)
+		{
+			printf("%s\n", ptr->d_name);
+
+			// create new path from our base path
+			strcpy(path, basePath);
+			strcat(path, "/");
+			strcat(path, ptr->d_name);
+
+			listFilesRecursively(path);
+
+		}
+	}
+		closedir(dir);
+}
+
