2fabfe11210a55fba4419ea15d166ba4a426117c
Daniel Tolentino <drt67@rutgers.edu>
2019-03-28 00:24:37 -0400

    openDIR change

--- openDIR.c
+++ openDIR.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
-#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
+#include <fnmatch.h>
+#include <fcntl.h>
+
+static char perms_buff[30];
+
+const char *get_perms(mode_t mode) 
+{ 
+char ftype = '?'; 
+if (S_ISREG(mode)) ftype = '-'; 
+if (S_ISLNK(mode)) ftype = 'l'; 
+if (S_ISDIR(mode)) ftype = 'd'; 
+if (S_ISBLK(mode)) ftype = 'b'; 
+if (S_ISCHR(mode)) ftype = 'c'; 
+if (S_ISFIFO(mode)) ftype = '|'; 
+sprintf(perms_buff, "%c%c%c%c%c%c%c%c%c%c %c%c%c", 
+ftype, 
+mode & S_IRUSR ? 'r' : '-', 
+mode & S_IWUSR ? 'w' : '-', 
+mode & S_IXUSR ? 'x' : '-', 
+mode & S_IRGRP ? 'r' : '-', 
+mode & S_IWGRP ? 'w' : '-', 
+mode & S_IXGRP ? 'x' : '-', 
+mode & S_IROTH ? 'r' : '-', 
+mode & S_IWOTH ? 'w' : '-', 
+mode & S_IXOTH ? 'x' : '-', 
+mode & S_ISUID ? 'U' : '-', 
+mode & S_ISGID ? 'G' : '-', 
+mode & S_ISVTX ? 'S' : '-'); 
+return (const char *)perms_buff; 
+} 
 
 void listFilesRecursively(char *path);
 
@@ -23,6 +53,9 @@ void listFilesRecursively(char *basePath) {
 	struct dirent *ptr;
 	DIR *dir = opendir(basePath);
 
+	struct stat statbuf;
+	struct passwd *pwd;
+
 	// Unable to open directory
 	if (!dir)
 		return;
@@ -32,37 +65,31 @@ void listFilesRecursively(char *basePath) {
 		if (strcmp(ptr->d_name,".") != 0 && strcmp(ptr->d_name, "..") != 0)
 		{
 
-			int length = strlen(ptr->d_name);
-			char *name = NULL;
-			name = malloc(sizeof(char) * length);
-			name = ptr->d_name;
-			char *type = NULL;
-			char c;
-			int x = 0, y = 0;
-			c = name[x];
-			
-			do {
-				c = name[x];
-				x++;
-			} while (c != "." || name[x] != NULL); //SEG FAULT IS IN THIS LINE - RUSS
-		
-			if (name[x] == NULL){
-				printf("%s\n", ptr->d_name);
-			} else {
-			
-			type = name;				
+			if (stat(ptr->d_name, &statbuf) == -1)
+        			continue;			
 
-			if (strcmp(type,"txt") == 0)			
-				printf("%s\n", ptr->d_name);
-			}
-			
-			free(name);
-			
+			 printf("%10.10s", get_perms (statbuf.st_mode));
+   			 printf("%4d", statbuf.st_nlink);
+
+			/*
+			if (fnmatch("*.txt",ptr->d_name,0) == 0) 
+				printf("%s\n",ptr->d_name);
+
+			if (fnmatch("*.c",ptr->d_name,0) == 0) 
+				printf("%s\n",ptr->d_name);
+		
+			if (fnmatch("*.h",ptr->d_name,0) == 0) 
+				printf("%s\n",ptr->d_name);
+
+			if (fnmatch("*.md",ptr->d_name,0) == 0) 
+				printf("%s\n",ptr->d_name);
+			*/
 
 			// create new path from our base path
 			strcpy(path, basePath);
 			strcat(path, "/");
 			strcat(path, ptr->d_name);
+			//printf("%s", ptr->d_name);
 
 			listFilesRecursively(path);
 
